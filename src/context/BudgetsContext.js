fed064eddfa4465a83f53bfd7a9b0f38562f34b2
Kaung Myat Tun <kaungtun111@gmail.com>
2022-11-05 15:47:58 +0630

    custom hooks

---
+++ src/context/BudgetsContext.js
@@ -0,0 +1,60 @@
+import React, { useContext } from "react"
+import { useState } from "react"
+import { v4 as uuidV4 } from 'uuid'
+import useLocalStorage from "../hooks/useLocalStorage"
+
+const BudgetsContext = React.createContext()
+
+export const useBudgets = () => {
+    return useContext(BudgetsContext)
+}
+
+export const BudgetsProvider = ({ children }) => {
+    const [budgets, setBudgets] = useLocalStorage('budgets', [])
+    const {expenses, setExpenses} = useLocalStorage('expenses', [])
+
+    const getBudgetExpenses = (budgetId) => {
+        return expenses.filter(expense => expense.budgetId === budgetId)
+    }
+
+    const addExpense = ({ description, amount, budgetId } ) => {
+        setBudgets(prevExpenses => {
+            return [...prevExpenses, { id: uuidV4(), description, amount, budgetId }]
+        })
+    }
+
+    const addBudget = (name, max) => {
+        setBudgets(prevBudgets => {
+            if (prevBudgets.name === name) {
+                return prevBudgets
+            }
+            return [...prevBudgets, { id: uuidV4(), name, max }]
+        })
+    }
+
+    const deleteBudget = ({ id }) => {
+        setBudgets(prevBudgets => {
+            return prevBudgets.filter(prevBudget => prevBudget.id !== id)
+        })
+    }
+
+    const deleteExpense = ({ id }) => {
+        setExpenses(prevExpenses => {
+            return prevExpenses.filter(prevExpense => prevExpense.id !== id)
+        })
+    }
+    
+    return (
+        <BudgetsContext.Provider value={{
+            budgets,
+            expenses,
+            getBudgetExpenses,
+            addExpense,
+            addBudget,
+            deleteBudget,
+            deleteExpense
+        }}>
+            {children}
+        </BudgetsContext.Provider>
+    )
+}
